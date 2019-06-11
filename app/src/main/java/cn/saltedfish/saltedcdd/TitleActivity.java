a03f5cedac78a6e2d88ddb69ca09d625d9c8cfb4
KeNorizon <32296555+KeNorizon@users.noreply.github.com>
2019-06-12 03:11:25 +0800

    add(activity): fullscreen activity support

--- app/src/main/java/cn/saltedfish/saltedcdd/TitleActivity.java
+++ app/src/main/java/cn/saltedfish/saltedcdd/TitleActivity.java
@@ -1,13 +1,12 @@
 package cn.saltedfish.saltedcdd;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import cn.saltedfish.saltedcdd.common.activity.FullscreenActivity;
 
-public class MainActivity extends AppCompatActivity {
-
+public class TitleActivity extends FullscreenActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_title);
     }
 }
